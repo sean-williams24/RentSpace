756d5f20e733f88d2666cd8aa3250dbcf7722f2d
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-11-29 14:12:58 +0000

    FEAT: Setup contact details VC with search bar for address search. Create table view controller and use as searchController for address search. Geocode address from search and format as postal address.

---
+++ RentSpace/View Controllers/AddressSearchTableViewController.swift
@@ -0,0 +1,135 @@
+//
+//  AddressSearchTableViewController.swift
+//  RentSpace
+//
+//  Created by Sean Williams on 29/11/2019.
+//  Copyright © 2019 Sean Williams. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import Contacts
+
+class AddressSearchTableViewController: UITableViewController, UISearchResultsUpdating, CLLocationManagerDelegate {
+    
+    var matchingItems: [MKMapItem] = []
+    var mapView: MKMapView? = nil
+    let formatter = CNPostalAddressFormatter()
+    let locationManager = CLLocationManager()
+    var userLocation = CLLocation()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        userLocation = locations.first!
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchBarText = searchController.searchBar.text else { return }
+        let request = MKLocalSearch.Request()
+        let region = MKCoordinateRegion(center: userLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 10000, longitudeDelta: 10000))
+
+        request.naturalLanguageQuery = searchBarText
+        request.region = region
+        
+        let search = MKLocalSearch(request: request)
+        search.start { response, error in
+            guard let response = response else {
+                print("Error: \(error?.localizedDescription ?? "Unkown Error")")
+                return
+            }
+            self.matchingItems = response.mapItems
+            self.tableView.reloadData()
+        }
+    }
+    
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return matchingItems.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AddressCell", for: indexPath)
+        let address = matchingItems[indexPath.row].placemark
+        cell.textLabel?.text = address.name
+        
+        if let postalAddress = address.postalAddress {
+            cell.detailTextLabel?.text = formatter.string(from: postalAddress).replacingOccurrences(of: "\n", with: ", ")
+        }
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let address = matchingItems[indexPath.row].placemark
+        if let postalAddress = address.postalAddress {
+            let formattedAddress = formatter.string(from: postalAddress)
+            ContactDetailsViewController.selectedAddress = formattedAddress
+        }
+        
+        dismiss(animated: true)
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
---
+++ RentSpace/View Controllers/ContactDetailsViewController.swift
@@ -0,0 +1,134 @@
+//
+//  ContactDetailsViewController.swift
+//  RentSpace
+//
+//  Created by Sean Williams on 29/11/2019.
+//  Copyright © 2019 Sean Williams. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ContactDetailsViewController: UIViewController {
+    
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var phoneNumberTextField: UITextField!
+    @IBOutlet var addressSearchButton: UIButton!
+    @IBOutlet var addressTextView: UITextView!
+    @IBOutlet var saveButton: UIButton!
+    @IBOutlet var searchBar: UISearchBar!
+    
+    let searchRequest = MKLocalSearch.Request()
+    var resultsSearchController: UISearchController?
+    static var selectedAddress = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        saveButton.layer.cornerRadius = 10
+        
+        let addressSearchTable = storyboard!.instantiateViewController(identifier: "AddressSearchTableVC") as! AddressSearchTableViewController
+        resultsSearchController = UISearchController(searchResultsController: addressSearchTable)
+        resultsSearchController?.searchResultsUpdater = addressSearchTable
+        
+        
+        let searchBar1 = resultsSearchController!.searchBar
+        searchBar1.sizeToFit()
+        searchBar1.placeholder = "Search Address"
+        navigationItem.titleView = resultsSearchController?.searchBar
+        
+        resultsSearchController?.hidesNavigationBarDuringPresentation = false
+        definesPresentationContext = true
+        
+    
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        addressTextView.text = ContactDetailsViewController.selectedAddress
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func addressTextFieldSearch(_ sender: UITextField) {
+        
+        
+    }
+    
+    
+    @IBAction func searchButtonTapped(_ sender: Any) {
+//        guard let address = addressTextField.text else { return }
+//        CLGeocoder().geocodeAddressString(address) { placemark, error in
+//
+//            if error != nil {
+//                 DispatchQueue.main.async {
+//                    // TODO: - Show error alert
+////                     self.showErrorAlert(title: "Unknown Location", error: "Problem finding location. Please try again.")
+//                 }
+//             }
+//
+//            if let placemarks = placemark {
+//                self.addressTextView.text = placemarks.first?.name
+//                print(placemarks)
+//                print(placemarks.count)
+//            }
+//        }
+        
+    }
+    
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+}
+
+
+// MARK: - Table View delegates
+//
+//extension ContactDetailsViewController: UITableViewDelegate, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "AddressCell", for: indexPath)
+//    }
+//
+//
+//
+//}
+
+
+
+// MARK: - Search bar delegates
+
+extension ContactDetailsViewController: UISearchBarDelegate {
+    
+//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        searchRequest.naturalLanguageQuery = searchBar.text
+//        let search = MKLocalSearch(request: searchRequest)
+//        
+//        search.start { response, error in
+//            guard let response = response  else {
+//                print("Error: \(error?.localizedDescription ?? "Unknown error").")
+//                return
+//            }
+//            for item in response.mapItems {
+//                print(item.placemark.name)
+//                self.addressTextView.text = item.placemark.name
+//            }
+//            
+//        }
+//    }
+    
+
+}
