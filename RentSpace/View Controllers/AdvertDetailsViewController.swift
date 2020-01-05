d2d353e63f2588dab7aab2cfaead1126517ec2f9
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2020-01-05 11:16:31 +0000

    FEAT: Create MySpaceDetails VC in IB, view controller and connect outlets.

--- RentSpace/View Controllers/AdvertDetailsViewController.swift
+++ RentSpace/View Controllers/AdvertDetailsViewController.swift
@@ -118,17 +118,7 @@ class AdvertDetailsViewController: UIViewController, UIScrollViewDelegate {
             }
         }
     }
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+
     
     //MARK: - Action Methods
 
