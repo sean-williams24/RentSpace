2cc0516cdd98c6139b45716fb11a1aa346d2d31b
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2020-02-09 15:46:34 +0000

    FEAT: Add weak self to update display name method to break strong reference cycle.

--- RentSpace/View Controllers/UpdateDetailsViewController.swift
+++ RentSpace/View Controllers/UpdateDetailsViewController.swift
@@ -33,10 +33,10 @@ class UpdateDetailsViewController: UIViewController {
     
     
     //MARK: - Lifecycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-                
+        
         title = userDetailToUpdate
         self.navigationController?.navigationBar.titleTextAttributes = Settings.navBarTitleAttributes
         
@@ -44,8 +44,8 @@ class UpdateDetailsViewController: UIViewController {
         addLeftPadding(for: confirmPasswordTextfield, placeholderText: "Confirm Password", placeholderColour: .darkGray)
         confirmPasswordTextfield.isHidden = true
         UID = Auth.auth().currentUser?.uid ?? ""
-
-
+        
+        
         if userDetailToUpdate == "Display Name" {
             updateTextfield.text = Auth.auth().currentUser?.displayName
             buttonGapConstraint.constant = 22
@@ -61,21 +61,58 @@ class UpdateDetailsViewController: UIViewController {
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        ref.child("users/\(UID)/adverts").removeObserver(withHandle: refHandle)
-
+        //        ref.child("users/\(UID)/adverts").removeObserver(withHandle: refHandle)
+        
     }
     
     
     //MARK: - Private Methods
-
-    fileprivate func handleUpdateCompletion(_ error: Error?) {
+    
+    fileprivate func handleUpdateCompletion(for type: String, for newCredential: String, _ error: Error?) {
         updateLoadingUI(false)
-
+        
         if error != nil {
             showAlert(title: "Oops!", message: error?.localizedDescription)
         } else {
             let ac = UIAlertController(title: "\(self.userDetailToUpdate!) Updated", message: nil, preferredStyle: .alert)
             ac.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+                
+                if type == "Display Name" {
+                    // Update user display name on all of users adverts
+                    self.refHandle = self.ref.child("users/\(self.UID)/adverts").observe(.value, with: { [weak self] (snapshot) in
+                        for child in snapshot.children {
+                            if let snapshot = child as? DataSnapshot,
+                                let space = Space(snapshot: snapshot) {
+                                
+                                self?.ref.child("users/\(self!.UID)/adverts").child(space.key).updateChildValues(["userDisplayName": newCredential])
+                                
+                            }
+                        }
+                    })
+                    
+                    // Update chats with new display name
+                    var index = 0
+                    self.ref.child("users/\(self.UID)/chats").observe(.value) { [weak self] (snapshot) in
+                        for child in snapshot.children {
+                            if let snapshot = child as? DataSnapshot,
+                                let chat = Chat(snapshot: snapshot) {
+                                
+                                if chat.advertOwnerUID == self?.UID {
+                                    self?.ref.child("users/\(self!.UID)/chats").child(chat.chatID).updateChildValues(["advertOwnerDisplayName": newCredential])
+                                    self?.ref.child("users/\(chat.customerUID)/chats").child(chat.chatID).updateChildValues(["advertOwnerDisplayName": newCredential])
+                                } else {
+                                    self?.ref.child("users/\(self!.UID)/chats").child(chat.chatID).updateChildValues(["customerDisplayName": newCredential])
+                                    self?.ref.child("users/\(chat.advertOwnerUID)/chats").child(chat.chatID).updateChildValues(["customerDisplayName": newCredential])
+                                }
+                                index += 1
+                                if index == snapshot.childrenCount {
+                                    return
+                                }
+                            }
+                        }
+                    }
+                }
+                
                 self.navigationController?.popViewController(animated: true)
             }))
             self.present(ac, animated: true)
@@ -103,7 +140,7 @@ class UpdateDetailsViewController: UIViewController {
     
     
     //MARK: - Action Methods
-
+    
     @IBAction func updateButtonTapped(_ sender: Any) {
         guard let newCredential = updateTextfield.text else { return }
         updateLoadingUI(true)
@@ -112,39 +149,8 @@ class UpdateDetailsViewController: UIViewController {
             if isValidDisplayName(newCredential) {
                 let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
                 changeRequest?.displayName = self.updateTextfield.text
-                changeRequest?.commitChanges { (error) in
-                    self.handleUpdateCompletion(error)
-                                        
-                    if error == nil {
-                        // Update user display name on all of users adverts
-                        self.refHandle = self.ref.child("users/\(self.UID)/adverts").observe(.value, with: { (snapshot) in
-                            for child in snapshot.children {
-                                if let snapshot = child as? DataSnapshot,
-                                    let space = Space(snapshot: snapshot) {
-                                    
-                                    self.ref.child("users/\(self.UID)/adverts").child(space.key).updateChildValues(["userDisplayName": newCredential])
-
-                                }
-                            }
-                        })
-                        
-                        // Update chats with new display name
-                        self.ref.child("users/\(self.UID)/chats").observe(.value) { (snapshot) in
-                            for child in snapshot.children {
-                                if let snapshot = child as? DataSnapshot,
-                                    let chat = Chat(snapshot: snapshot) {
-                                    
-                                    if chat.advertOwnerUID == self.UID {
-                                        self.ref.child("users/\(self.UID)/chats").child(chat.chatID).updateChildValues(["advertOwnerDisplayName": newCredential])
-                                        self.ref.child("users/\(chat.customerUID)/chats").child(chat.chatID).updateChildValues(["advertOwnerDisplayName": newCredential])
-                                    } else {
-                                        self.ref.child("users/\(self.UID)/chats").child(chat.chatID).updateChildValues(["customerDisplayName": newCredential])
-                                        self.ref.child("users/\(chat.advertOwnerUID)/chats").child(chat.chatID).updateChildValues(["customerDisplayName": newCredential])
-                                    }
-                                }
-                            }
-                        }
-                    }
+                changeRequest?.commitChanges { [weak self] (error) in
+                    self?.handleUpdateCompletion(for: "Display Name", for: newCredential, error)
                 }
             } else {
                 updateLoadingUI(false)
@@ -153,8 +159,9 @@ class UpdateDetailsViewController: UIViewController {
         } else if userDetailToUpdate == "Email" {
             if isValidEmail(newCredential) {
                 Auth.auth().currentUser?.updateEmail(to: newCredential) { (error) in
-                    self.handleUpdateCompletion(error)
-                   }
+                    self.handleUpdateCompletion(for: "Email", for: newCredential, error)
+                    
+                }
             } else {
                 updateLoadingUI(false)
                 showAlert(title: "Oh No!", message: "That doesn't appear to be a valid email address...")
@@ -162,7 +169,7 @@ class UpdateDetailsViewController: UIViewController {
         } else {
             if isValidPassword(newCredential) && newCredential == confirmPasswordTextfield.text {
                 Auth.auth().currentUser?.updatePassword(to: newCredential) { (error) in
-                  self.handleUpdateCompletion(error)
+                    self.handleUpdateCompletion(for: "Password", for: newCredential, error)
                 }
             } else {
                 updateLoadingUI(false)
